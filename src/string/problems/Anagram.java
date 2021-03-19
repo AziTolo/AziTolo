ecef3df6ca6e80e3aaf1d8f8108693e7567a5fc4
= <=>
2021-03-19 11:30:35 -0400

    Commit message

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,14 +1,35 @@
 package string.problems;
+import java.util.Arrays;
+import java.util.Locale;
 
 public class Anagram {
 
-    /*
-    Write a Java Program to check if two Strings are Anagrams.
-        Two String are called Anagrams when both Strings use the same characters but in different order.
-        Example: "CAT" and "ACT", "ARMY" and "MARY", "FART" and "RAFT"
-    */
 
     public static void main(String[] args) {
+        String str1="Race";
+        String str2="Care";
+
+        str1= str1.toLowerCase();
+        str2=str2.toLowerCase();
+
+        if (str1.length()==str2.length()){
+
+           char[] charArray1= str1.toCharArray();
+           char[] charArray2= str2.toCharArray();
+
+           Arrays.sort(charArray1);
+           Arrays.sort(charArray2);
+           boolean result=Arrays.equals(charArray1,charArray2);
+
+           if (result){
+               System.out.println(str1+" and "+str2+ " are anagram.");
+           }
+           else {
+               System.out.println(str1+" and "+str2+" :are not anagram.");
+           }
+        }
+
+
 
     }
 }
