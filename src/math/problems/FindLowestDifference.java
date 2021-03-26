f856e04d6b1ec4ae92aa262645fc964626e45dd3
= <=>
2021-03-26 11:34:25 -0400

    Commit message;

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -1,26 +1,65 @@
 package math.problems;
 
+import java.util.Arrays;
+
 public class FindLowestDifference {
 
 
+
+
+    static int lowestDifference(int array1[], int array2[], int m, int n){
+
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+
+        int a = 0;
+        int b = 0;
+
+        int result = Integer.MAX_VALUE;
+
+        while (a < m && b < n){
+
+            if (Math.abs(array1[a] - array2[b]) < result)
+                result = Math.abs(array1[a] - array2[b]);
+
+            if (array1[a] < array2[b])
+                a++;
+            else
+                b++;
+        }
+        return result;
+    }
+
+
+
+
     public static void main(String[] args) {
-        /*
-         Write a method to return the the lowest difference between these two arrays
-            HINT: The lowest difference between these arrays is 1
-        */
+/*
+Write a method to return the the lowest difference between these two arrays
+HINT: The lowest difference between these arrays is 1
+*/
 
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19};
 
+        int m = array1.length;
+        int n = array2.length;
 
-    }
-    public static void FindLowestDifference(int[] array1,int[] array2){
-        int lowestDifference;
-        int lowestInArray1=array1[0];
-        int lowestInArray2=array2[0];
-
+        System.out.println(lowestDifference(array1,array2,m,n));
 
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
